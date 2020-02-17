e6565fede971498ca0ffc7babadf1aadaf07106e
Mateusz <matstanczak@gmail.com>
2020-02-17 21:14:03 +0000

    icons added

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -4,14 +4,21 @@ const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
-        return lat > 0 ? 'winter' : 'summer'
+        return lat > 0 ? 'winter' : 'summer';
     }
 }
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season)
-    return <div>Season Display</div>
+    const displayText = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+    const icon = season === 'winter' ? 'snowflake' : 'sun'
+return (
+        <div>
+            <i className={`${icon} icon` } />
+            <h1>{displayText}</h1>
+            <i className={`${icon} icon`} />
+        </div>
+    )
 }
 
 export default SeasonDisplay;
